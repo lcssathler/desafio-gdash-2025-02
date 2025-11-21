6f4a8c27b37c51c9cd71277d065d82fb57625ff7
lcssathler <lcssathler@gmail.com>
2025-11-21 12:20:53 -0300

    add: user service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ConflictException, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './schemas/user.schema';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  async create(email: string, password: string, name: string) {
+    const exists = await this.userModel.findOne({ email });
+    if (exists) throw new ConflictException('Email já cadastrado');
+
+    const hashed = await bcrypt.hash(password, 10);
+    const user = new this.userModel({ email, password: hashed, name, role: email.includes('admin') ? 'admin' : 'user' });
+    return user.save();
+  }
+
+  async findByEmail(email: string) {
+    return this.userModel.findOne({ email });
+  }
+
+}
